676be490dfde530e8088d1fc42f88d4c512e498e
Nilswall <117596161+Nilswall@users.noreply.github.com>
2024-01-30 15:12:44 +0100

    Update TransportTest.java

--- lab2/test/src/TransportTest.java
+++ lab2/test/src/TransportTest.java
@@ -3,6 +3,9 @@ package src;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.awt.geom.Point2D;
+
+import static java.lang.Math.sqrt;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransportTest {
@@ -27,13 +30,13 @@ class TransportTest {
     @org.junit.jupiter.api.Test
     void changeAngleWhileDriving(){
         transport.startEngine();
-        Exception y = assertThrows(RuntimeException.class, () -> transport.changeAngle(10));
+        Exception y = assertThrows(RuntimeException.class, () -> transport.changeAngle(90));
         assertEquals(y.getMessage(), "Cant change angle while driving");
     }
 
     @org.junit.jupiter.api.Test
     void startDriveWithAngle() {
-        transport.changeAngle(10);
+        transport.changeAngle(90);
         Exception y = assertThrows(RuntimeException.class, () -> transport.startEngine());
         assertEquals(y.getMessage(), "Cant start if flaks angle not 0");
     }
@@ -41,6 +44,10 @@ class TransportTest {
 
     @Test
     void avståndsformeln() {
+        Point2D point1 = new Point2D.Double(0, 0);
+        Point2D point2 = new Point2D.Double(1, 1);
+        double dist = transport.avståndsformeln(point1, point2);
+        assertEquals(dist, sqrt(2));
     }
 
     @org.junit.jupiter.api.Test
@@ -66,7 +73,21 @@ class TransportTest {
         assertEquals(y.getMessage(), "Cant load without ramp down");
     }
 
-    @Test
-    void offLoadCargo() {
+    @org.junit.jupiter.api.Test
+    void offLoadCargoWhileDrive() {
+        Car car = new Saab95();
+        transport.loadCargo(car);
+        transport.startEngine();
+        Exception y = assertThrows(RuntimeException.class, () -> transport.offLoadCargo());
+        assertEquals(y.getMessage(), "Cant offload while driving");
+    }
+
+    @org.junit.jupiter.api.Test
+    void offloadCargoWhileAngle() {
+        Car car = new Saab95();
+        transport.loadCargo(car);
+        transport.changeAngle(90);
+        Exception y = assertThrows(RuntimeException.class, () -> transport.offLoadCargo());
+        assertEquals(y.getMessage(), "Cant offload without ramp down");
     }
 }
